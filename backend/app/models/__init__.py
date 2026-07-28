82dca14c94aca55914c89ec6c06447efd97cabee
Vaishnavi-MS-36 <vm731197@gmail.com>
2026-07-28 08:44:26 +0530

    Fix SQLAlchemy mapper error - register all models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,16 @@
+from app.models.user import User
+from app.models.table import RestaurantTable
+from app.models.category import Category
+from app.models.menu_item import MenuItem
+from app.models.ingredient import Ingredient
+from app.models.recipe import Recipe
+from app.models.supplier import Supplier
+from app.models.purchase_order import PurchaseOrder, PurchaseOrderItem
+from app.models.order import Order, OrderItem
+from app.models.invoice import Invoice
+from app.models.expense import Expense, ExpenseCategory
+from app.models.stock_movement import StockMovement
+from app.models.product import Product, ProductCategory
+from app.models.warehouse import Warehouse
+from app.models.notification import Notification
+from app.models.audit_log import AuditLog
